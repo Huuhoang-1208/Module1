22f41f16e77e20b395ab1060dc2cd5939aab645c
Huuhoang-1208 <hoang.nguyen230401@vnuk.edu.vn>
2024-11-15 08:08:42 -0500

    nop bai tap 7

---
+++ ss7/chuyen_doi_tien_te.js
@@ -0,0 +1,18 @@
+function changeMoney() {
+    let Amount = document.getElementById('Amount').value;
+    let From = document.getElementById('From').value;
+    let To = document.getElementById('To').value;
+    let Result;
+
+    if (From == "USD"&&To == "VND"){
+        Result = 'Result :' + Amount * 24 +'VND';
+    }
+    else if (From == "VND"&&To == "VND" ){
+        Result = 'Result :' + Amount +'VND';
+    }else if(From == "USD"&&To == "USD"){
+        Result = 'Result :' + Amount +'USD';
+    }else{
+        Result = 'Result :' + Amount/24 +'USD';
+    }
+    document.getElementById(    "Result").innerHTML = Result;
+}
