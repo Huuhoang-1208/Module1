357bf417dc55d951a61aa2607f0cb861d96244ac
Huuhoang-1208 <hoang.nguyen230401@vnuk.edu.vn>
2024-11-29 09:30:24 -0500

    nop bai tap

---
+++ ss11/bai_tap/Luyen_tap_vong_lap_1/bai_2/bai_2.js
@@ -0,0 +1,6 @@
+  let nhietdo = parseFloat(prompt("Nhập nhiệt độ hiện tại:"));
+      if (nhietdo < 20 ){
+          alert("Tăng nhiệt độ");
+      } else if (nhietdo>100); {
+          alert("Giảm nhiệt độ");
+  }
